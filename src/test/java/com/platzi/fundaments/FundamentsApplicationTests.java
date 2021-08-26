4dcc78c5b15fd84883693a7b1c20ff409f7fef07
Homero Villanueva <hfidelvillanueva@gmail.com>
2021-08-26 17:37:36 -0500

    first commit

---
+++ src/test/java/com/platzi/fundaments/FundamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.fundaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
